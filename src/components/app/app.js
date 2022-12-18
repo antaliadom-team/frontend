c2057f4dba4cea7f280a05e285dbcc629ea1ffdd
Resaw <resavv@mail.ru>
2022-12-18 04:12:30 +0300

    инициализация проекта

---
+++ src/components/app/app.js
@@ -0,0 +1,27 @@
+import logo from '../../logo.svg';
+import styles from  './app.module.css';
+import { Home } from "../../pages";
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className={styles.link}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import styles from "./home.module.css"
+
+
+const Home = () => {
+    return (
+        <h1 className={styles.header}>
+           Home page
+        </h1>
+    );
+};
+
+export default Home;

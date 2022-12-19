28de38962088ae96c14ae4cda01513f66b47820b
Igor Manukhov <m_igor97@mail.ru>
2022-12-19 21:15:04 +0300

    css styles import

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,25 +1,8 @@
-import logo from '../../logo.svg';
-import styles from './App.css';
-import { Home } from "../../pages";
 
-const App = () => {
+function App() {
   return (
-    <div className={styles.app}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Home />
-      </header>
+    <div className='App'>
+
     </div>
   );
 }

b4ac69235c8f4fcf34c16baf59a3d9daabb6fce5
Resaw <resavv@mail.ru>
2022-12-21 04:58:58 +0300

    написал роутинг

---
+++ src/pages/about/about.js
@@ -0,0 +1,7 @@
+import styles from "./about.module.css";
+
+const About = () => {
+    return <h1 className={styles.title}>About page</h1>;
+};
+
+export default About;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,7 @@
 import styles from "./home.module.css";
 
 const Home = () => {
-  return <h1 className={styles.header}>Home page</h1>;
+  return <h1 className={styles.title}>Home page</h1>;
 };
 
 export default Home;

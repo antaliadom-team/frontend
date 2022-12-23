1b0ea83be948e133010d2c56e651d850626490d6
Igor Manukhov <m_igor97@mail.ru>
2022-12-23 17:19:27 +0300

    add fonts

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -21,29 +21,31 @@ const Main = () => {
       </section>
 
       <section className={styles.advantages}>
-        <div className={styles.columns}>
-          <div className={styles.column}>
-            <h3 className={styles.about}>О нас</h3>
-            <p className={styles.paragraph_about}>
-              Мы поможем вам найти квартиру мечты в Анталье.
-              <br />В нашей базе собрано более 200 актуальных
-              <br />
-              объявлений о сдаче и продаже недвижимости в Анталье.
-              <br />А если вы не найдете ничего подходящего, наш
-              <br />
-              менеджер свяжется с вами и поможет подобрать что-то
-              <br />
-              на ваш вкус
-            </p>
-            <button className={styles.button}>Смотреть каталог</button>
-          </div>
-          <div className={styles.column}>
-            <img
-              className={styles.advantageImage}
-              src={advantageImage}
-              alt="фото квартиры"
-            />
-            <div className={styles.blockUnderImage} />
+        <div className={styles.advantages_container}>
+          <div className={styles.columns}>
+            <div className={styles.column}>
+              <h3 className={styles.about}>О нас</h3>
+              <p className={styles.paragraph_about}>
+                Мы поможем вам найти квартиру мечты в Анталье.
+                <br />В нашей базе собрано более 200 актуальных
+                <br />
+                объявлений о сдаче и продаже недвижимости в Анталье.
+                <br />А если вы не найдете ничего подходящего, наш
+                <br />
+                менеджер свяжется с вами и поможет подобрать что-то
+                <br />
+                на ваш вкус
+              </p>
+              <button className={styles.button}>Смотреть каталог</button>
+            </div>
+            <div className={styles.column}>
+              <img
+                className={styles.advantageImage}
+                src={advantageImage}
+                alt="фото квартиры"
+              />
+              <div className={styles.blockUnderImage} />
+            </div>
           </div>
         </div>
       </section>

3418bfb221e9617c4ed8149dc75195ffb3838df5
Igor Manukhov <m_igor97@mail.ru>
2022-12-23 22:56:11 +0300

    добавлена секция заявки

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,6 +5,10 @@ import photo_2 from "../../images/photo_2.png";
 import photo_3 from "../../images/photo_3.png";
 import photo_4 from "../../images/photo_4.png";
 import aboutImage from "../../images/about_main.png";
+import advantage1 from "../../images/advantage1.svg";
+import advantage2 from "../../images/advantage2.svg";
+import advantage3 from "../../images/advantage3.svg";
+import checkbox from "../../images/checkbox.svg";
 
 const Main = () => {
   return (
@@ -95,6 +99,117 @@ const Main = () => {
             <p className={styles.aboutTeam__number}>+90 (534) 123 45 67</p>
           </li>
         </ul>
+      </section>
+      <section className={styles.advantages}>
+        <h2 className={styles.advantages__title}>Наши преимущества</h2>
+        <div className={styles.advantages__container}>
+          <div className={styles.advantages__item}>
+            <img
+              className={styles.advantages__image}
+              src={advantage1}
+              alt="картинка преимущества команды"
+            />
+            <div className={styles.advantages__line1} />
+          </div>
+          <div className={styles.advantages__item}>
+            <img
+              className={styles.advantages__image}
+              src={advantage2}
+              alt="картинка преимущества команды"
+            />
+            <div className={styles.advantages__line2} />
+          </div>
+          <div className={styles.advantages__item}>
+            <img
+              className={styles.advantages__image}
+              src={advantage3}
+              alt="картинка преимущества команды"
+            />
+            <div className={styles.advantages__line3} />
+          </div>
+        </div>
+      </section>
+      <section className={styles.hotAdvertisement}>
+        <h2 className={styles.hotAdvertisement__title}>Свежие объявления</h2>
+        <ui className={styles.hotAdvertisement__container}>
+          <li className={styles.hotAdvertisement__testCard}></li>
+          <li className={styles.hotAdvertisement__testCard}></li>
+          <li className={styles.hotAdvertisement__testCard}></li>
+          <li className={styles.hotAdvertisement__testCard}></li>
+        </ui>
+        <button className={styles.hotAdvertisement__moreButton}>
+          Смотреть все
+        </button>
+      </section>
+      <section className={styles.application}>
+        <div className={styles.application__container}>
+          <div className={styles.application__block} />
+          <h2 className={styles.application__title}>
+            Оставить заявку на подбор недвижимости
+          </h2>
+          <p className={styles.application__subtitle}>
+            Пожалуйста, заполните ваши данные и мы свяжемся с Вами в ближайшее
+            время
+          </p>
+          <form className={styles.application__form}>
+            <ui className={styles.application__items}>
+              <li className={styles.application__item}>
+                <p className={styles.application__itemTitle}>Имя*</p>
+                <input className={styles.application__input} />
+              </li>
+              <li className={styles.application__item}>
+                <p className={styles.application__itemTitle}>Фамилия*</p>
+                <input className={styles.application__input} />
+              </li>
+              <li className={styles.application__item}>
+                <p className={styles.application__itemTitle}>Номер телефона*</p>
+                <input className={styles.application__input} />
+              </li>
+              <li className={styles.application__item}>
+                <p className={styles.application__itemTitle}>Email*</p>
+                <input className={styles.application__input} />
+              </li>
+              <li className={styles.application__item}>
+                <p className={styles.application__itemTitle}>Аренда/Покупка</p>
+                <input className={styles.application__input} />
+              </li>
+              <li className={styles.application__item}>
+                <p className={styles.application__itemTitle}>Область</p>
+                <input className={styles.application__input} />
+              </li>
+              <li className={styles.application__item}>
+                <p className={styles.application__itemTitle}>
+                  Тип недвижимости
+                </p>
+                <input className={styles.application__input} />
+              </li>
+              <li className={styles.application__item}>
+                <p className={styles.application__itemTitle}>
+                  Количество комнат
+                </p>
+                <input className={styles.application__input} />
+              </li>
+              <li className={styles.application__item}>
+                <p className={styles.application__itemTitle}>Комментарий</p>
+                <input className={styles.application__input_comment} />
+              </li>
+            </ui>
+            <div className={styles.application__checkbox}>
+              <img
+                className={styles.application__checkboxImage}
+                src={checkbox}
+                alt="чекбокс"
+              />
+              <p className={styles.application__accept}>
+                Я согласен с Политикой конфиденциальности и Условиями
+                использования сервиса
+              </p>
+            </div>
+            <button className={styles.application__send}>
+              Отправить заявку
+            </button>
+          </form>
+        </div>
       </section>
     </div>
   );

f1d8a3d3900ed5c6eb07ee50a96459cba83eac11
Igor Manukhov <m_igor97@mail.ru>
2022-12-25 14:32:37 +0300

    о нас тянется по ширине

--- src/components/main/about/about.js
+++ src/components/main/about/about.js
@@ -12,29 +12,33 @@ const About = () => {
   return (
     <>
       <section className={styles.aboutUs}>
-        <div className={styles.aboutUs__columns}>
-          <div className={styles.aboutUs__column}>
-            <h2 className={styles.aboutUs__title}>О нас</h2>
-            <p className={styles.aboutUs__paragraph}>
-              Мы поможем вам найти квартиру мечты в Анталье.
-              <br />В нашей базе собрано более 200 актуальных
-              <br />
-              объявлений о сдаче и продаже недвижимости в Анталье.
-              <br />А если вы не найдете ничего подходящего, наш
-              <br />
-              менеджер свяжется с вами и поможет подобрать что-то
-              <br />
-              на ваш вкус
-            </p>
-            <button className={styles.aboutUs__button}>Смотреть каталог</button>
-          </div>
-          <div className={styles.aboutUs__column}>
-            <img
-              className={styles.aboutUs__image}
-              src={aboutImage}
-              alt="фото квартиры"
-            />
-            <div className={styles.aboutUs__blockUnderImage} />
+        <div className={styles.aboutUs__container}>
+          <div className={styles.aboutUs__columns}>
+            <div className={styles.aboutUs__column}>
+              <h2 className={styles.aboutUs__title}>О нас</h2>
+              <p className={styles.aboutUs__paragraph}>
+                Мы поможем вам найти квартиру мечты в Анталье.
+                <br />В нашей базе собрано более 200 актуальных
+                <br />
+                объявлений о сдаче и продаже недвижимости в Анталье.
+                <br />А если вы не найдете ничего подходящего, наш
+                <br />
+                менеджер свяжется с вами и поможет подобрать что-то
+                <br />
+                на ваш вкус
+              </p>
+              <button className={styles.aboutUs__button}>
+                Смотреть каталог
+              </button>
+            </div>
+            <div className={styles.aboutUs__column}>
+              <img
+                className={styles.aboutUs__image}
+                src={aboutImage}
+                alt="фото квартиры"
+              />
+              <div className={styles.aboutUs__blockUnderImage} />
+            </div>
           </div>
         </div>
       </section>

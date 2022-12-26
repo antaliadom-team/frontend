7391bbc45bbab2214cbf7e803cb4e87f93a18689
Igor Manukhov <m_igor97@mail.ru>
2022-12-26 23:07:46 +0300

    fix conflicts git

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,17 +2,22 @@ import styles from "./app.module.css";
 import { Routes, Route } from "react-router-dom";
 import { About, Catalog, Home, Profile } from "../../pages";
 import Header from "../header/header";
+import Footer from "../footer/footer";
 
 const App = () => {
   return (
     <div className={styles.app}>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 };

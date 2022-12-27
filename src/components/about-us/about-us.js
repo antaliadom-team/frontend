4b0e836a3297326634c647884993e34b40d2be1c
Igor Manukhov <m_igor97@mail.ru>
2022-12-27 22:27:33 +0300

    home переделал под конечный макет

--- src/components/about-us/about-us.js
+++ src/components/about-us/about-us.js
@@ -1,5 +1,6 @@
 import styles from "./about-us.module.css";
-import aboutImage from "../../images/about_main.png";
+import aboutImageRight from "../../images/about-us-right.png";
+import aboutImageLeft from "../../images/about-us-left.png";
 
 const AboutUs = () => {
   return (
@@ -9,20 +10,75 @@ const AboutUs = () => {
           <h2 className={styles.aboutUs__title}>О нас</h2>
           <p className={styles.aboutUs__paragraph}>
             <span>Мы знаем</span>, что аренда, покупка или продажа недвижимости
-            часто может быть непростым и трудоемким процессом, полным
-            неожиданных сюрпризов. От скрытых проблем с квартирами до
-            непорядочных брокеров — мы видели и слышали это на протяжении многих
-            лет.
+            часто может быть непростым
           </p>
-          <button className={styles.aboutUs__button}>Смотреть каталог</button>
+          <p className={styles.aboutUs__paragraph}>
+            и трудоемким процессом, полным неожиданных сюрпризов. От скрытых
+            проблем с квартирами
+          </p>
+          <p className={styles.aboutUs__paragraph}>
+            до непорядочных брокеров — мы видели и слышали это на протяжении
+            многих лет.
+          </p>
+          <p className={styles.aboutUs__paragraph}>
+            Вот почему миссия <span>«АнталияДом»</span> заключается в том, чтобы
+            перевернуть типичный опыт работы
+          </p>
+          <p className={styles.aboutUs__paragraph}>
+            с недвижимостью, сделав этот процесс приятным
+          </p>
+          <p className={styles.aboutUs__paragraph}>
+            для наших клиентов. Команда компании состоит
+          </p>
+          <p className={styles.aboutUs__paragraph}>
+            из реальных людей с реальным опытом, сочетающих в себе
+            профессионализм и знание нюансов рынка.
+          </p>
         </div>
         <div className={styles.aboutUs__column}>
           <img
             className={styles.aboutUs__image}
-            src={aboutImage}
+            src={aboutImageRight}
             alt="фото квартиры"
           />
           <div className={styles.aboutUs__border} />
+        </div>
+      </div>
+      <div className={styles.aboutUs__columns}>
+        <div className={styles.aboutUs__column}>
+          <img
+            className={styles.aboutUs__image}
+            src={aboutImageLeft}
+            alt="фото квартиры"
+          />
+          <div
+            className={`${styles.aboutUs__border} ${styles.aboutUs__border_bottom}`}
+          />
+        </div>
+        <div
+          className={`${styles.aboutUs__column} ${styles.aboutUs__column_bottom}`}>
+          <p className={styles.aboutUs__paragraph}>
+            <span>Мы понимаем,</span> вы принимаете чрезвычайно важное решение и
+            вам нужны люди, которым
+          </p>
+          <p
+            className={`${styles.aboutUs__paragraph} ${styles.aboutUs__paragraph_bottom}`}>
+            вы можете доверять.
+          </p>
+          <p
+            className={`${styles.aboutUs__paragraph} ${styles.aboutUs__paragraph_bottom}`}>
+            <span>Мы поможем</span> вам найти идеальное место
+          </p>
+          <p
+            className={`${styles.aboutUs__paragraph} ${styles.aboutUs__paragraph_bottom}`}>
+            и позаботимся о том, чтобы вы связали с ним будущее.
+          </p>
+          <p
+            className={`${styles.aboutUs__paragraph} ${styles.aboutUs__paragraph_bottom}`}>
+            <span>Мы хотим,</span> чтобы вы любили место, где живете. Это
+            желание управляет всем, что мы делаем.
+          </p>
+          <button className={styles.aboutUs__button}>Смотреть каталог</button>
         </div>
       </div>
     </section>

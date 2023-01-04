f533ca96552777bd92f2087c5440229b5a4a1d72
Igor Manukhov <m_igor97@mail.ru>
2023-01-04 12:09:35 +0300

    добавил отступы в редактировать проыиль

--- src/components/forms/auth-form.js
+++ src/components/forms/auth-form.js
@@ -8,39 +8,41 @@ import question from "../../images/ic_question.svg";
 const EditProfile = () => {
   return (
     <form className={styles.form}>
-      <h2 className={styles.title}>Редактировать профиль</h2>
-      <Link className={styles.link} to="/auth/change-password">
-        Изменить пароль
-      </Link>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <p className={styles.itemTitle}>Ваше имя*</p>
-          <input type="text" placeholder="Иван" className={styles.input} />
-        </li>
-        <li className={styles.item}>
-          <p className={styles.itemTitle}>Ваша фамилия*</p>
-          <input placeholder="Иванов" className={styles.input} />
-        </li>
-        <li className={styles.item}>
-          <p className={styles.itemTitle}>Ваш e-mail*</p>
-          <input
-            type="text"
-            placeholder="ivanov@mail.ru"
-            className={styles.input}
-          />
-        </li>
-        <li className={styles.item}>
-          <p className={styles.itemTitle}>Номер телефона*</p>
-          <input
-            type="text"
-            placeholder="+7 921 123 45 67"
-            className={styles.input}
-          />
-        </li>
-      </ul>
-      <div className={styles.buttons}>
-        <button>Сохранить</button>
-        <button>Отменить</button>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Редактировать профиль</h2>
+        <Link className={styles.link} to="/auth/change-password">
+          Изменить пароль
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <p className={styles.itemTitle}>Ваше имя*</p>
+            <input type="text" placeholder="Иван" className={styles.input} />
+          </li>
+          <li className={styles.item}>
+            <p className={styles.itemTitle}>Ваша фамилия*</p>
+            <input placeholder="Иванов" className={styles.input} />
+          </li>
+          <li className={styles.item}>
+            <p className={styles.itemTitle}>Ваш e-mail*</p>
+            <input
+              type="text"
+              placeholder="ivanov@mail.ru"
+              className={styles.input}
+            />
+          </li>
+          <li className={styles.item}>
+            <p className={styles.itemTitle}>Номер телефона*</p>
+            <input
+              type="text"
+              placeholder="+7 921 123 45 67"
+              className={styles.input}
+            />
+          </li>
+        </ul>
+        <div className={styles.buttons}>
+          <button>Сохранить</button>
+          <button>Отменить</button>
+        </div>
       </div>
     </form>
   );

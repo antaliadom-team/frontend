d9dacb49283bbe5882e7c767a9d9788592553da2
Resaw <resavv@mail.ru>
2023-01-08 18:42:38 +0300

    Поправил параграфы в блоке "О нас"

--- src/components/about-us/about-us.js
+++ src/components/about-us/about-us.js
@@ -10,28 +10,16 @@ const AboutUs = () => {
           <h2 className={styles.aboutUs__title}>О нас</h2>
           <p className={styles.aboutUs__paragraph}>
             <span>Мы знаем</span>, что аренда, покупка или продажа недвижимости
-            часто может быть непростым
-          </p>
-          <p className={styles.aboutUs__paragraph}>
-            и трудоемким процессом, полным неожиданных сюрпризов. От скрытых
-            проблем с квартирами
-          </p>
-          <p className={styles.aboutUs__paragraph}>
-            до непорядочных брокеров — мы видели и слышали это на протяжении
-            многих лет.
+            часто может быть непростым и&nbsp;трудоемким процессом, полным
+            неожиданных сюрпризов. От скрытых проблем с квартирами
+            до&nbsp;непорядочных брокеров — мы видели и слышали это на
+            протяжении многих лет.
           </p>
           <p className={styles.aboutUs__paragraph}>
             Вот почему миссия <span>«АнталияДом»</span> заключается в том, чтобы
-            перевернуть типичный опыт работы
-          </p>
-          <p className={styles.aboutUs__paragraph}>
-            с недвижимостью, сделав этот процесс приятным
-          </p>
-          <p className={styles.aboutUs__paragraph}>
-            для наших клиентов. Команда компании состоит
-          </p>
-          <p className={styles.aboutUs__paragraph}>
-            из реальных людей с реальным опытом, сочетающих в себе
+            перевернуть типичный опыт работы с&nbsp;недвижимостью, сделав этот
+            процесс приятным для&nbsp;наших клиентов. Команда компании состоит
+            из&nbsp;реальных людей с реальным опытом, сочетающих в себе
             профессионализм и знание нюансов рынка.
           </p>
         </div>
@@ -58,23 +46,16 @@ const AboutUs = () => {
         <div
           className={`${styles.aboutUs__column} ${styles.aboutUs__column_bottom}`}>
           <p className={styles.aboutUs__paragraph}>
-            <span>Мы понимаем,</span> вы принимаете чрезвычайно важное решение и
-            вам нужны люди, которым
+            <span>Мы понимаем,</span> вы принимаете чрезвычайно важное решение
+            и&nbsp; вам нужны люди, которым вы можете доверять.
           </p>
           <p
-            className={`${styles.aboutUs__paragraph} ${styles.aboutUs__paragraph_bottom}`}>
-            вы можете доверять.
+            className={styles.aboutUs__paragraph}>
+            <span>Мы поможем</span> вам найти идеальное место и&nbsp;позаботимся о
+            том, чтобы вы связали с ним будущее.
           </p>
           <p
-            className={`${styles.aboutUs__paragraph} ${styles.aboutUs__paragraph_bottom}`}>
-            <span>Мы поможем</span> вам найти идеальное место
-          </p>
-          <p
-            className={`${styles.aboutUs__paragraph} ${styles.aboutUs__paragraph_bottom}`}>
-            и позаботимся о том, чтобы вы связали с ним будущее.
-          </p>
-          <p
-            className={`${styles.aboutUs__paragraph} ${styles.aboutUs__paragraph_bottom}`}>
+            className={styles.aboutUs__paragraph}>
             <span>Мы хотим,</span> чтобы вы любили место, где живете. Это
             желание управляет всем, что мы делаем.
           </p>

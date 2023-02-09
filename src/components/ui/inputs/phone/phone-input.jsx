4c44d6a53896653806ea208588f26d558797df5e
Resaw <resavv@mail.ru>
2023-02-10 02:39:49 +0300

    установил библиотеку для маски, добавил выбор кода региона

--- src/components/ui/inputs/phone/phone-input.jsx
+++ src/components/ui/inputs/phone/phone-input.jsx
@@ -1,55 +1,54 @@
 import React, { useRef, useState } from "react";
+import MaskedInput from "react-text-mask";
 import styles from "./phone.module.css";
 
 const PhoneInput = ({ text, placeholder, disabled = false }) => {
-
   const inputRef = useRef(null);
-  const [value, setValue] = useState("");
   const [error, setError] = useState(false);
   const [warning, setWarning] = useState("");
+  const [dropdown, setDropdown] = useState(false);
+  const [selected, setSelected] = useState("+7");
+  const options = ["+7", "+90"];
 
-  const formattedPhoneNumber = (value) => {
-    if (!value) return;
-    const phoneNumber = value.replace(/[^\d]/g, '');
-    const phoneNumberLength = phoneNumber.length;
-    if (phoneNumberLength < 4) {
-      return `+ ${phoneNumber}`;
-    } if (phoneNumberLength < 5 ) {
-      return `+ ${phoneNumber.slice(0, 1)} ${phoneNumber.slice(1)}`;
+  const validate = () => {
+    const value = inputRef.current.inputElement.value.match(/\d+/g).join("");
+    if (value.length < 10) {
+      setError(true);
     } else {
-      return `+ ${phoneNumber.slice(0, 1)} (${phoneNumber.slice(1, 4)}) ${phoneNumber.slice(4)}`;
-    }
-  };
-
-  const validate = (value) => {
-    const phoneLength = value.length;
-    if (!value.length) { return };
-    if (phoneLength > 14 && phoneLength < 19) {
       setError(false);
       setWarning("");
-    } else {
-      setError(true);
-    };
-  };
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setValue(formattedPhoneNumber(e.target.value))
-    validate(value);
+    }
   };
 
   return (
     <div className={styles.wrapper}>
       <label className={styles.field}>
         <span className={styles.text}>{text}</span>
-        <input
+        <div className={styles.region_code} onClick={() => setDropdown(!dropdown)}>
+          {selected}
+          <span className={styles.arrow}></span>
+        </div>
+        {dropdown && (
+          <div className={styles.region_options}>
+            {options.map((option) => (
+              <div
+                onClick={() => {
+                  setSelected(option);
+                  setDropdown(!dropdown);
+                }}
+                className={styles.region_item}>
+                {option}
+              </div>
+            ))}
+          </div>
+        )}
+        <MaskedInput
+          mask={["(", /[1-9]/, /\d/, /\d/, ")", " ", /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]}
           className={!warning ? styles.input : styles.warning}
-          type="text"
           ref={inputRef}
-          onChange={handleChange}
-          onBlur={()=> error && setWarning(styles.warning)}
-          value={value}
-          placeholder={disabled ? null : placeholder}
+          onChange={validate}
+          onBlur={() => error && setWarning(styles.warning)}
+          placeholder={disabled ? null : "(999) 123-4567"}
           disabled={disabled}
         />
         <span className={error ? styles.error : styles.hide}>Введите корректный номер телефона</span>

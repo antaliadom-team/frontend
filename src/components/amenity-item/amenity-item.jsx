3094540c2146d99a0c385f82f6e2ed5726ce080a
Igor Manukhov <m_igor97@mail.ru>
2023-03-06 13:00:33 +0300

    расширил список удобств

--- src/components/amenity-item/amenity-item.jsx
+++ src/components/amenity-item/amenity-item.jsx
@@ -2,22 +2,13 @@ import styles from "./amenity-item.module.css";
 import { Parking, Wifi, Ac, Wash } from "../ui/icons";
 
 const AmenityItem = ({ title, isAvailable }) => {
-  const getClassName = (isAvailable) =>
-    isAvailable ? styles.amenities_item : styles.amenities_item_disabled;
+  const getClassName = (isAvailable) => (isAvailable ? styles.amenities_item : styles.amenities_item_disabled);
 
   switch (title) {
     case "Интернет": {
       return (
         <li className={getClassName(isAvailable)}>
           <Wifi isAvailable={isAvailable} />
-          {title}
-        </li>
-      );
-    }
-    case "Парковка": {
-      return (
-        <li className={getClassName(isAvailable)}>
-          <Parking isAvailable={isAvailable} />
           {title}
         </li>
       );
@@ -38,10 +29,84 @@ const AmenityItem = ({ title, isAvailable }) => {
         </li>
       );
     }
+    case "Паркинг": {
+      return (
+        <li className={getClassName(isAvailable)}>
+          <Parking isAvailable={isAvailable} />
+          {title}
+        </li>
+      );
+    }
+    case "Газовое отопление": {
+      return (
+        <li className={getClassName(isAvailable)}>
+          <Parking isAvailable={isAvailable} />
+          {title}
+        </li>
+      );
+    }
+    case "Тренажерный зал": {
+      return (
+        <li className={getClassName(isAvailable)}>
+          <Parking isAvailable={isAvailable} />
+          {title}
+        </li>
+      );
+    }
+    case "Бассейн": {
+      return (
+        <li className={getClassName(isAvailable)}>
+          <Parking isAvailable={isAvailable} />
+          {title}
+        </li>
+      );
+    }
+    case "Сауна": {
+      return (
+        <li className={getClassName(isAvailable)}>
+          <Parking isAvailable={isAvailable} />
+          {title}
+        </li>
+      );
+    }
+    case "Мебель": {
+      return (
+        <li className={getClassName(isAvailable)}>
+          <Parking isAvailable={isAvailable} />
+          {title}
+        </li>
+      );
+    }
+    case "Охраняемая территория": {
+      return (
+        <li className={getClassName(isAvailable)}>
+          <Parking isAvailable={isAvailable} />
+          {title}
+        </li>
+      );
+    }
+    case "Балкон": {
+      return (
+        <li className={getClassName(isAvailable)}>
+          <Parking isAvailable={isAvailable} />
+          {title}
+        </li>
+      );
+    }
+    case "Посудомоечная машина": {
+      return (
+        <li className={getClassName(isAvailable)}>
+          <Parking isAvailable={isAvailable} />
+          {title}
+        </li>
+      );
+    }
     default: {
       return <li className={getClassName(isAvailable)}>{title}</li>;
     }
   }
 };
+
+console.log("паркинг - где на макете?, бассейн - где на макете?");
 
 export default AmenityItem;

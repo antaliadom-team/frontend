dd78b59b553f175863a8f6c29ded60b5d07d5ebf
Bella Bzhukova <104579797+bellabzhu@users.noreply.github.com>
2023-03-16 00:34:14 +0300

    настроила обновление имени и фамилии пользователя

--- src/components/forms/edit-profile/edit-profile.jsx
+++ src/components/forms/edit-profile/edit-profile.jsx
@@ -1,43 +1,52 @@
 import styles from "./edit-profile.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import React, { useState, useContext } from "react";
+import { useForm, Controller } from "react-hook-form";
 import { PhoneInput, TextInput } from "../../ui/inputs";
 import { updateUser } from "../../../services/api/user";
 import { Button } from "../../ui/buttons";
 import { ModalContext, UserContext } from "../../../services/app-context";
+import {
+  nameValidation,
+  phoneValidation,
+  surnameValidation,
+} from "../../../services/validation";
 
 const EditProfile = ({setUser}) => {
   const { modal, setModal } = useContext(ModalContext);
   const { user } = useContext(UserContext);
-  const phoneMasked = `(${user.phone.substring(2,5)}) ${user.phone.substring(5)}`
-  const [ formData, setFormData ] = useState({
-    first_name: user.first_name,
-    last_name: user.last_name,
-    phone: user.phone,
-  })
-  console.log(formData.first_name)
+  //const phoneMasked = `(${user.phone.substring(2,5)}) ${user.phone.substring(5)}`
+  // const [ formData, setFormData ] = useState({
+  //   first_name: user.first_name,
+  //   last_name: user.last_name,
+  //   phone: user.phone,
+  // })
+  //console.log(formData.first_name)
+
+  const {
+    control,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "all",
+  });
+
 
   const navigate = useNavigate();
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    //const { first_name, last_name, phone } = e.target.elements;
+  const onSubmit = (data) => {
     console.log('you submitted the form!')
-
-    // сейчас не видит value, надо передавать его сюда в компонент
+    console.log(data);
     updateUser({
-      first_name: "Gtnz",
-      last_name: "Хахахах",
-      phone: "+79112349809",
+      first_name: data.first_name,
+      last_name: data.last_name,
+      phone: data.phone,
     }, setUser);
-  };
-
-  const handleChange = (e) => {
-    console.log(e)
   };
   
   return (
-    <form className={styles.form} onSubmit={submitForm}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <div className={styles.container}>
         <h2 className={styles.title}>Редактировать профиль</h2>
         <Link className={styles.link} to="/edit-password">
@@ -45,21 +54,71 @@ const EditProfile = ({setUser}) => {
         </Link>
         <ul className={styles.list}>
           <li>
-            <TextInput name={"first_name"} label="Ваше имя*" handleChange={handleChange} placeholder={formData.first_name} />
+            <Controller
+                control={control}
+                name="first_name"
+                rules={nameValidation}
+                render={({ field, fieldState }) => (
+                  <TextInput 
+                    name={"first_name"} 
+                    label="Ваше имя*"
+                    placeholder={user.first_name} 
+                    onChange={(e) => field.onChange(e)}
+                    value={field.value}
+                    error={fieldState.error}
+                    errorText={errors.first_name?.message}
+                  />
+                )}
+            />
           </li>
           <li>
-            <TextInput name={"last_name"} label="Ваша фамилия*" handleChange={handleChange} placeholder={formData.last_name} />
+            <Controller
+              control={control}
+              name="last_name"
+              rules={nameValidation}
+              render={({ field, fieldState }) => (
+                <TextInput 
+                  name={"last_name"} 
+                  label="Ваша фамилия*"
+                  placeholder={user.last_name}
+                  onChange={(e) => field.onChange(e)}
+                  value={field.value} 
+                  error={fieldState.error}
+                  errorText={errors.first_name?.message}
+                />
+              )}
+            />
           </li>
           <li>
-            <TextInput name={"email"} label="Ваш e-mail*" disabled={true} readOnly={true} value={user.email} />
+            <TextInput 
+              name={"email"} 
+              label="Ваш e-mail*" 
+              disabled={true} 
+              readOnly={true} 
+              value={user.email}
+            />
           </li>
           <li>
-            <PhoneInput name={"phone"} text="Номер телефона*" handleChange={handleChange} currentPhone={formData.phone} />
+            <PhoneInput 
+              name={"phone"} 
+              text="Номер телефона*"
+              currentPhone={user.phone} 
+            />
           </li>
         </ul>
         <div className={styles.buttons}>
-          <Button isSubmit={true} type="primary" onClick={() => setModal({ ...modal, passwordChanged: true })}>Сохранить</Button>
-          <Button type="ghost" onClick={() => navigate(-1)}>Отменить</Button>
+          <Button 
+            isSubmit={true} 
+            type="primary" 
+            onClick={() => setModal({ ...modal, passwordChanged: true })}
+            >
+            Сохранить
+          </Button>
+          <Button 
+            type="ghost" 
+            onClick={() => navigate(-1)}
+            >Отменить
+          </Button>
         </div>
       </div>
     </form>

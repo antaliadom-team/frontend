621243a086ffd4e2cd0bd2cb22bfa0522e4e4014
Resaw <resavv@mail.ru>
2023-03-22 06:26:17 +0300

    исправил компонент ads

--- src/components/ads/ads.jsx
+++ src/components/ads/ads.jsx
@@ -2,19 +2,21 @@ import styles from "./ads.module.css";
 import CatalogItem from "../catalog-item/catalog-item";
 import { Button } from "../ui/buttons";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { ObjectsContext } from "../../services/app-context";
 
 const Ads = () => {
-  // фикс компонента для свежих объявлений на главной странице
   const navigate = useNavigate();
+  const { objects } = useContext(ObjectsContext);
 
   return (
     <section className={styles.ads}>
       <h2 className={styles.title}>Свежие объявления</h2>
       <div className={styles.wrapper}>
-        <CatalogItem />
-        <CatalogItem />
-        <CatalogItem />
-        <CatalogItem />
+        {objects?.results ? objects?.results.map((item, index) => {
+          if (index > 3) return null;
+          return <CatalogItem key={item.id} item={item} />
+        }) : (<h2 className={styles.title}>Здесь пока пусто</h2>)}
       </div>
 
       <div className={styles.button}>

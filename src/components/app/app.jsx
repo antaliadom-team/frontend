ad05909f5f30a11af0b820a2ea14dc3ebe50d477
Resaw <resavv@mail.ru>
2023-03-23 20:28:45 +0300

    теперь при переходе по страницам скролл всегда в начале, в модалках отключен скролл за модалкой

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,11 @@
 import styles from "./app.module.css";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { Catalog, Home, Object, Profile, Order } from "../../pages";
 import { Register, Logout, EditProfile, EditPassword, Login } from "../forms";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import { AuthContext, ModalContext, UserContext } from "../../services/app-context";
-import { useContext } from "react";
+import { useContext, useLayoutEffect } from "react";
 import ProtectedRoute from "../protected-route/protected-route";
 import Layout from "../layout/layout";
 import { logoutUser } from "../../services/api/user";
@@ -13,8 +13,9 @@ import { useScrollToLocation } from "../../hooks/use-scroll";
 import { SliderModal, PasswordModal, ExitModal, ObjectModal, Policy, Submit } from "../modals";
 
 const App = () => {
-    const { modal, setModal } = useContext(ModalContext);
     const navigate = useNavigate();
+    const { pathname } = useLocation();
+    const { modal, setModal } = useContext(ModalContext);
     const { setAuth } = useContext(AuthContext);
     const { setUser } = useContext(UserContext);
 
@@ -28,7 +29,7 @@ const App = () => {
         navigate("/");
     };
 
-    if (modal.object || modal.exit) {
+    if (modal.object || modal.exit || modal.passwordChanged || modal.policy || modal.slider || modal.submit) {
         const x = window.scrollX;
         const y = window.scrollY;
         window.onscroll = () => {
@@ -39,6 +40,10 @@ const App = () => {
     }
 
     useScrollToLocation();
+
+    useLayoutEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
 
     return (
         <div className={styles.app}>

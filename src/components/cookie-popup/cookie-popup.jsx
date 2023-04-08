883ce0603932028d8e6411aa151da99d7e124d52
Resaw <resavv@mail.ru>
2023-04-08 09:23:51 +0300

    поправил попап с куками

--- src/components/cookie-popup/cookie-popup.jsx
+++ src/components/cookie-popup/cookie-popup.jsx
@@ -1,10 +1,10 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../../services/app-context";
-import styles from "./cookiePopup.module.css";
+import styles from "./cookie-popup.module.css";
 import { Button } from "../ui/buttons";
 
 const CookiePopup = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
     const { isAuth } = useContext(AuthContext);
     const closePopup = () => {
         setIsOpen(false);
@@ -13,15 +13,15 @@ const CookiePopup = () => {
     if (isAuth) return;
     if (isOpen) {
         return (
-            <div className={styles.cookiePopup}>
-                <p className={styles.cookiePopup__text}>
+            <div className={styles.popup}>
+                <p className={styles.text}>
                     При использовании данного сайта, вы подтверждаете свое согласие на использование файлов cookie и
                     других похожих технологий в соответствии с настоящим Уведомлением
                 </p>
                 <Button onClick={closePopup} type="primary">
                     Ок
                 </Button>
-                <button onClick={closePopup} className={styles.cookie__closeBtn}></button>
+                <button onClick={closePopup} className={styles.close}></button>
             </div>
         );
     }
